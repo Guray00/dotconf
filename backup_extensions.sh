07be4d146bee14719b5afc4772a5ddfec9ed8648
Marco <30523962+Guray00@users.noreply.github.com>
2021-06-02 18:31:16 +0200

    Update backup_extensions.sh

--- backup_extensions.sh
+++ backup_extensions.sh
@@ -2,11 +2,12 @@
 cd /tmp/dotconf
 
 # removing backup if exists
-rm -f /tmp/dotconf/extensions.tar
-rm -f /tmp/dotconf/extension-settings.dconf
+rm -Rf /tmp/dotconf/extensions
+
+mkdir /tmp/dotconf/extensions
 
 # creating a zip file with infos
-tar -cvf extensions.tar ~/.local/share/gnome-shell/extensions/
+tar -cvf ./extensions/extensions.tar ~/.local/share/gnome-shell/extensions/
 
 # backing up dconf
-dconf dump /org/gnome/shell/extensions/ > extension-settings.dconf
+dconf dump /org/gnome/shell/extensions/ > ./extensions/extension-settings.dconf
