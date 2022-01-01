202cddc4ec59580ee55a14fa9ed5eb65c2ab45d2
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2022-01-01 13:08:27 +0100

    Update bluetooth.sh

--- scripts/bluetooth.sh
+++ scripts/bluetooth.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo pacman -S --needed bluez bluez-utils pipewire-pulse blueman
+sudo pacman -S --needed --noconfirm bluez bluez-utils pipewire-pulse blueman
 sudo systemctl enable --now bluetooth
