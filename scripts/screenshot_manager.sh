b131c781b4bca2bb73403034b2442f30791b0024
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2022-01-01 13:07:59 +0100

    Update screenshot_manager.sh

--- scripts/screenshot_manager.sh
+++ scripts/screenshot_manager.sh
@@ -3,7 +3,7 @@
 # CHANGES SCREENSHOT MANAGER
 # reference: https://askubuntu.com/questions/1036473/how-to-change-screenshot-application-to-flameshot-on-ubuntu-18-04/1039949#1039949
 
-yay -S --noconfirm --quiet flameshot
+yay -S --noconfirm --quiet --needed flameshot
 gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot '[]'
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
