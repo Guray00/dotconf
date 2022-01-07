e3172a2d6a6329cf0b749340c8fbb8708a59500c
Marco Lampis <30523962+Guray00@users.noreply.github.com>
2022-01-07 16:46:27 +0100

    enable themes
    Migliorato installer che abilita in automatico i temi installati

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -11,6 +11,21 @@ rm -Rf /tmp/dotconf/.icons
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme /tmp/dotconf/.icons
 /tmp/dotconf/.icons/install.sh
 
+# cursor
+rm -Rf /tmp/dotconf/.cursor
+git clone https://github.com/vinceliuice/WhiteSur-cursors /tmp/dotconf/.cursor
+/tmp/dotconf/.cursor/install.sh
+
 # removes junk
 rm -Rf /tmp/dotconf/.theme
 rm -Rf /tmp/dotconf/.icons
+rm -Rf /tmp/dotconf/.cursor
+
+# abilito l'estensione dei temi
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+
+# abilito i setup
+gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark-solid"  # imposta il tema delle applicazioni
+gsettings set org.gnome.desktop.interface icon-theme "WhiteSur"            # imposta le icone
+gsettings set org.gnome.desktop.interface cursor-theme "WhiteSur-cursors"  # imposta il cursore
+gsettings set org.gnome.shell.extensions.user-theme name "WhiteSur-dark"   # imposta la shell
